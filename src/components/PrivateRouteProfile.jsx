123c5048b2ee40e009fee7db1fb813984923e75b
OkkashaAlly <steamnature006@gmail.com>
2022-04-28 17:43:28 +0300

    ProtectedRoute-profilePage

--- src/components/PrivateRouteProfile.jsx
+++ src/components/PrivateRouteProfile.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
 export const PrivateRouteProfile = () => {
-  const loggedIn = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) return <h3>Loading...</h3>;
 
   return loggedIn ? <Outlet /> : <Navigate to={"/sign-in"} />;
 };

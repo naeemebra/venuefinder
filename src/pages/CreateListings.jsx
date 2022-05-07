9e79b22df6bcfc348b453a63f1571fdfb5929265
OkkashaAlly <steamnature006@gmail.com>
2022-05-07 16:28:32 +0300

    Create listings

--- src/pages/CreateListings.jsx
+++ src/pages/CreateListings.jsx
@@ -1,11 +1,22 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/shared/Spinner";
+import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CreateListings() {
   const [loading, setLoading] = useState(false);
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  // const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const geolocationEnabled = false;
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -60,8 +71,104 @@ export default function CreateListings() {
 
   if (loading) return <Spinner />;
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price has to be less than regular price");
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images");
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+
+      console.log(process.env.REACT_APP_GEOCODE_API_KEY);
+      const data = await response.json();
+
+      console.log(data);
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+
+    // store images in firebase
+    const storeImage = async image => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, "images/" + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          snapshot => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                console.log("err");
+            }
+          },
+          error => {
+            reject(error);
+          },
+          _ => {
+            getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map(image => storeImage(image))
+    ).catch(_ => {
+      setLoading(false);
+      toast.error("Failed to upload images");
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      geolocation,
+      imgUrls,
+      timestamp: serverTimestamp(),
+    };
+
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    location && (formDataCopy.location = location);
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+
+    setLoading(false);
+    toast.success('Lisiting Created')
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`)
   };
 
   const handleClick = e => {
@@ -91,7 +198,7 @@ export default function CreateListings() {
       </header>
       <main>
         <form onSubmit={handleSubmit}>
-          <lable className="formLabel">Sell / Rent</lable>
+          <label className="formLabel">Sell / Rent</label>
           <div className="formButtons">
             <button
               type="button"
